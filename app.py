938f2bedc80ba8677670257b22ab13a89dd46d03
Adeniyi Olaolu <adeniyiolaolu016@gmail.com>
2024-07-21 20:12:06 +0100

    commit

--- app.py
+++ app.py
@@ -40,11 +40,13 @@ def predict():
             'income_group_num': float(data['income_group_num']),
             'year': float(data['year']),
             'el_access_total': float(data['el_access_total']),
-            'gdp_pc': float(data['gdp_pc']),
-            't_demand': float(data['t_demand']),
-            'supply_rate': float(data['el_demand']) / float(data['el_access_total'])  # Calculated feature
+            'gdp_pc': float(data['gdp_pc'])
         }
-        
+
+        # Calculate supply_rate and t_demand
+        features['supply_rate'] = features['el_demand'] / features['el_access_total']
+        features['t_demand'] = 100 * features['supply_rate']
+
         logging.debug(f"Extracted features: {features}")
 
         # Prepare features for prediction
@@ -63,10 +65,10 @@ def predict():
             features['year'],
             features['el_access_total'],
             features['gdp_pc'],
-            features['t_demand'],
-            features['supply_rate']
+            features['supply_rate'],
+            features['t_demand']
         ]
-        
+
         logging.debug(f"Feature values before scaling: {feature_values}")
 
         # Ensure feature_values has the correct number of features
@@ -77,18 +79,18 @@ def predict():
         # Scale the features
         feature_values = np.array(feature_values).reshape(1, -1)
         feature_values_scaled = scaler.transform(feature_values)
-        
+
         logging.debug(f"Scaled feature values: {feature_values_scaled}")
 
         # Reshape for LSTM
         feature_values_scaled = feature_values_scaled.reshape((feature_values_scaled.shape[0], 1, feature_values_scaled.shape[1]))
-        
+
         logging.debug(f"Reshaped feature values for LSTM: {feature_values_scaled}")
 
         # Make the prediction
         prediction = model.predict(feature_values_scaled)
         prediction = np.abs(prediction[0][0])
-        
+
         logging.debug(f"Prediction: {prediction}")
 
         # Return the prediction
